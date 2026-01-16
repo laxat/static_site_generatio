f3a087467fbde6c14330805f2ea17a399d6da15c
laxat <fatman7100@gmail.com>
2026-01-16 05:29:17 +0000

    added markdown blocks and testcases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,14 +26,14 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, props=props)
 
-    def __repr__(self):
-        return self.to_html()
+    # def __repr__(self):
+    #     return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
     def to_html(self):
         if not self.value:
             raise ValueError("All leaf nodes must have a value")
         if not self.tag:
-            return self.value()
+            return self.value
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
--- src/main.py
+++ src/main.py
@@ -1,20 +1,42 @@
-from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type, block_type_to_html, markdown_to_html
 
 
 def main():
+    block = "# heading"
+
+    type = block_to_block_type(block)
+    html_node = block_type_to_html(type, block)
+
+    block1 = "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line"
+    type1 = block_to_block_type(block1)
+
+    html_node_0 = block_type_to_html(type1, block1)
+
+    # print(html_node)
+    # print(html_node_0.value)
+    md = """```
+This is text that _should_ remain
+the **same** even with inline stuff
+```"""
+
+    ans = markdown_to_html(md)
+    # print(repr(ans.to_html()))
+
     md = """
-    This is **bolded** paragraph
-
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
-
     - This is a list
     - with items
+    - and _more_ items
+
+    1. This is an `ordered` list
+    2. with items
+    3. and more items
+
     """
 
-    blocks = markdown_to_blocks(md)
+    node = markdown_to_html(md)
+    # html = node.to_html()
 
-    print(blocks)
+    print(node.to_html())
 
 
 main()
